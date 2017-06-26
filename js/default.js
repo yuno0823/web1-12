da4c6bf42b3874eb9058e0ca5e028041cb923db9
yuno0823 <g1627024@st.t-kougei.ac.jp>
2017-06-26 18:05:26 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -1,10 +1,4 @@
-for(var i = 1; i < 101; i++){
-var li = document.createElement('li');
-li.textContent = i + '回目';
-document.getElementById('list').appendChild(li);
-}
-
 document.getElementById('form').onsubmit = function(){
-window.alert(document.getELementById('form').word.value);
+window.alert(document.getElementById('form').word.value);
   return false;
 };
