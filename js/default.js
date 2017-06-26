b264c1b7daa4ca9d2fde44bf0342c6e694172cd3
yuno0823 <g1627024@st.t-kougei.ac.jp>
2017-06-26 17:58:32 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -1,7 +1,7 @@
 for(var i = 1; i < 101; i++){
 var li = document.createElement('li');
 li.textContent = i + '回目';
-document.getELementById('list').appendChild(li);
+document.getElementById('list').appendChild(li);
 }
 
 document.getElementById('form').onsubmit = function(){
